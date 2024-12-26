fb4f46319a353493a5ce1354eeefb2b55a946121
Elrevmore <155190474+Elrevmore@users.noreply.github.com>
2024-12-27 02:00:35 +0300

    Create einstein.py

---
+++ Problem set 0 /einstein.py
@@ -0,0 +1,8 @@
+def main():
+    kilogram = int(input("Please Enter mass in kilograms: "))
+    speed_of_light = 300000000
+    energy = kilogram * (speed_of_light **2)
+    print(f"The equivalent Energy is {energy} Joules.")
+
+
+main()
